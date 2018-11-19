d5b9c0683aa3aa58eeb6895306c40e066e14ae37
Taras Rushchak <taraskhust@gmail.com>
2018-11-19 16:22:12 +0200

    add alert

--- lesson 5/js/script.js
+++ lesson 5/js/script.js
@@ -1,7 +1,29 @@
 let menu = document.querySelector('.menu');
+let h1 = document.getElementById("title");
+let item = document.querySelectorAll('li');
 let li = document.createElement('li');
 let text = document.createTextNode('Пятый пункт');
+let adv = document.querySelectorAll('.column');
+let adv1 = document.querySelector('.adv');
+let body = document.querySelector('body');
+let prom = document.getElementById('prompt');
+
+let timer = () => {
+    let question = prompt('Как вам техника Apple', '');
+    prom.innerHTML = question;
+};
+
+let timerId = setTimeout(timer, 3000);
+
 
 li.classList.add('menu-item');
 menu.appendChild(li).appendChild(text);
-console.log(li);
+menu.insertBefore(item[2], item[1]);
+
+h1.textContent = 'Мы продаем только подлинную технику Apple';
+
+adv[1].removeChild(adv1);
+
+body.style = 'background: url(./img/apple_true.jpg) center no-repeat';
+
+
